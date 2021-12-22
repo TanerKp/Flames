ea124f0335c984ed90f7266f52253fa857468de2
taner <kp.taner@gmail.com>
2021-12-22 01:35:52 +0100

    improved player code

---
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Player
+{
+    public class PlayerBehavior : MonoBehaviour
+    {
+        /*
+         *  Todo:
+         *  -   Create deathSound
+         */
+
+        /* PUBLIC VARIABLES */
+        public GameObject particleDeath;
+        public Animator cameraAnimator;
+
+        /* PRIVATE VARIABLES */
+        private AudioSource _audio;
+        private HealthBar _health;
+
+
+        /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+         *  UNITY FUNCTIONS
+         */
+
+        private void Start()
+        {
+            _audio = GetComponent<AudioSource>();
+            _health = GetComponent<HealthBar>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            OnCollideWithTraps(collision);
+            OnCollideWithEnemy(collision);
+        }
+
+
+        /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+         *  PRIVATE FUNCTIONS
+         */
+
+        /* On collide with enemy traps */
+        private void OnCollideWithTraps(Collider2D c)
+        {
+            if (!c.CompareTag("Traps")) return;
+
+            // Takes 1 damage from player
+            DamagePlayer();
+        }
+
+        /* On collide with enemy */
+        private void OnCollideWithEnemy(Collider2D c)
+        {
+            if (!c.CompareTag("Enemy")) return;
+
+            // Only attack if enemy is in the near of the player
+            if (c.transform.position.y < transform.position.y - 0.75f)
+            {
+                DamagePlayer();
+            }
+
+            // Enemy explodes after attacking player
+            c.GetComponent<HealthBar>().isDead = true;
+        }
+
+        /* Checks if player has health otherwise it kills him */
+        private void CheckPlayerHealth()
+        {
+            if (!_health.isDead) return;
+            Destroy(gameObject);
+        }
+
+        /* Gives player damage */
+        void DamagePlayer()
+        {
+            // Activates camera shake effect
+            ShakeCamera();
+            
+            // Takes 1 hearth from his health
+            _health.TakeDamage(1);
+            
+            // Creates particles
+            Instantiate(particleDeath, transform.position, Quaternion.identity);
+            
+            // Plays sound
+            _audio.Play();
+            
+            // Checks if play has enough health
+            CheckPlayerHealth();
+        }
+
+        
+        // Todo: Export Function to a new Class for CameraActions
+        void ShakeCamera()
+        {
+            cameraAnimator.SetTrigger("shake");
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,81 +1,95 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class PlayerController : MonoBehaviour {
-
-    /* PUBLIC VARIABLES */
-    public float playerSpeed;
-
-    /* PRIVATE VARIABLES */
-    private Animator playerAnimator;
-    private Rigidbody2D playerRigidbody2D;
-    private Vector2 playerMoveVelocity;
-    private bool playerFacingRight = false;
-
-    /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-     *  UNITY FUNCTIONS
-     */
-    
-    private void Start()
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
     {
-        playerAnimator = GetComponent<Animator>();
-        playerRigidbody2D = GetComponent<Rigidbody2D>();
-    }
+        /* PUBLIC VARIABLES */
+        public float playerSpeed;
 
-    private void Update()
-    {
-        playerMoveVelocity = HandleMoveInput();
-        FacingPlayerToMouseDirection();
-    }
-    
-    private void FixedUpdate()
-    {
-        playerRigidbody2D.MovePosition(playerRigidbody2D.position + playerMoveVelocity * Time.fixedDeltaTime);
-    }
-    
-    /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-     *  PRIVATE FUNCTIONS
-     */
-    
-    // Handles user input for player movement (Axis-Input : WASD)
-    private Vector2 HandleMoveInput()
-    {
-        // Creates new Vector2 with player input
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        
-        // Activates player-animation if player is moving
-        playerAnimator.speed = moveInput.magnitude;
-        
-        // Returns normalized input (1 or -1) multiplied with playerSpeed
-        return moveInput.normalized * playerSpeed;
-    }
+        /* PRIVATE VARIABLES */
+        private Animator _playerAnimator;
+        private Rigidbody2D _playerRigidbody2D;
+        private Vector2 _playerMoveVelocity;
+        private bool _playerFacingRight = false;
+        private Camera _playerCamera;
 
-    // Facing player to the mouse direction
-    private void FacingPlayerToMouseDirection()
-    {
-        float mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-        if (mousePosition < transform.position.x && playerFacingRight)
+        /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+         *  UNITY FUNCTIONS
+         */
+
+        private void Start()
         {
-            Flip();
+            _playerAnimator = GetComponent<Animator>();
+            _playerRigidbody2D = GetComponent<Rigidbody2D>();
+            _playerCamera = Camera.main;
         }
-        if (mousePosition > transform.position.x && !playerFacingRight)
+
+        private void Update()
         {
-            Flip();
+            _playerMoveVelocity = HandleMoveInput();
+            FacingPlayerToMouseDirection();
+        }
+
+        private void FixedUpdate()
+        {
+            _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _playerMoveVelocity * Time.fixedDeltaTime);
+        }
+
+        /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+         *  PRIVATE FUNCTIONS
+         */
+
+        /* Handles user input for player movement (Axis-Input : WASD) */
+        private Vector2 HandleMoveInput()
+        {
+            // Creates new Vector2 with player input
+            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            // Activates player-animation if player is moving
+            _playerAnimator.speed = moveInput.magnitude;
+
+            // Returns normalized input (1 or -1) multiplied with playerSpeed
+            return moveInput.normalized * playerSpeed;
+        }
+
+        /* Facing player to the mouse direction */
+        private void FacingPlayerToMouseDirection()
+        {
+            try
+            {
+                var mousePosition = _playerCamera.ScreenToWorldPoint(Input.mousePosition).x;
+                if (mousePosition < transform.position.x && _playerFacingRight)
+                {
+                    Flip();
+                }
+
+                if (mousePosition > transform.position.x && !_playerFacingRight)
+                {
+                    Flip();
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        /* Flips the facing direction of the player */
+        private void Flip()
+        {
+            _playerFacingRight = !_playerFacingRight;
+
+            var playerTransform = transform;
+
+            Vector3 theScale = playerTransform.localScale;
+            theScale.x *= -1;
+            playerTransform.localScale = theScale;
         }
     }
-
-    // Flips the facing direction of the player
-    private void Flip()
-    {
-        playerFacingRight = !playerFacingRight;
-
-        var playerTransform = transform;
-        
-        Vector3 theScale = playerTransform.localScale;
-        theScale.x *= -1;
-        playerTransform.localScale = theScale;
-    }
-    
 }
